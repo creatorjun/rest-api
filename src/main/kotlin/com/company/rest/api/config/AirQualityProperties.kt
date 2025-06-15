97f06b6c40456e15e1c72ef289194042dc941807
Jun <createbrain2heart@gmail.com>
2025-06-15 23:07:32 +0900

    완료

--- src/main/kotlin/com/company/rest/api/config/AirQualityProperties.kt
+++ src/main/kotlin/com/company/rest/api/config/AirQualityProperties.kt
@@ -3,7 +3,7 @@ package com.company.rest.api.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "airkorea")
+@ConfigurationProperties(prefix = "kapi")
 data class AirQualityProperties(
     @Value("\${kapi.service.key}")
     val serviceKey: String,
--- src/main/kotlin/com/company/rest/api/config/HolidayApiProperties.kt
+++ src/main/kotlin/com/company/rest/api/config/HolidayApiProperties.kt
@@ -3,7 +3,7 @@ package com.company.rest.api.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties(prefix = "holiday-api")
+@ConfigurationProperties(prefix = "kapi")
 data class HolidayApiProperties(
     @Value("\${kapi.service.key}")
     val serviceKey: String,
